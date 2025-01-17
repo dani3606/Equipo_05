235d915be101ac006fca58177ed7b82d0b2d3eb6
dani3606 <dcazgon@g.educaand.es>
2025-01-17 14:39:47 +0100

    version1 Daniel

---
+++ src/Equipo05/Testdividir3.java
@@ -0,0 +1,8 @@
+package Equipo05;
+
+public class Main {
+	System.out.println("3 es divisible entre 3" + Main.dividir3(3));
+	System.out.println("3 es divisible entre 3" + Main.dividir3(0));
+	System.out.println("3 es divisible entre 3" + Main.dividir3(8));
+	System.out.println("3 es divisible entre 3" + Main.dividir3(9));
+}
