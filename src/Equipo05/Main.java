ff3c589eab250c08e3f5980029aff6da41cb3668
AdrianDelgadoMontoro <adelmon801@g.educaand.es>
2025-01-17 13:37:58 +0200

    Version 1 Adrian

--- src/Equipo05/Main.java
+++ src/Equipo05/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
+		
 
 	}
 
+	public static boolean esPar (int numero){
+		return numero %2 == 0;
+	}
 }
---
+++ src/Equipo05/Pruebas.java
@@ -0,0 +1,12 @@
+	package Equipo05;
+
+	public class Pruebas {
+
+		public static void main(String[] args) {
+			System.out.println(Main.esPar(2));
+			System.out.println(Main.esPar(3));
+			System.out.println(Main.esPar(4));
+			System.out.println(Main.esPar(0));
+
+		}
+	}
