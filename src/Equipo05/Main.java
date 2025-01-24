32640c50769ae07977ba5ba2be8b7fb2c9e6b2d2
AdrianDelgadoMontoro <adelmon801@g.educaand.es>
2025-01-24 13:38:01 +0100

    Version funcional Equipo05

--- src/Equipo05/Main.java
+++ src/Equipo05/Main.java
@@ -1,22 +1,57 @@
 package Equipo05;
 
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Hola mundo");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean salir = false;
 
-		
-	}
+        while (!salir) {
+            System.out.println("Introduce un número: ");
+            int numero = scanner.nextInt();
 
-	public static boolean dividir3(int numero){
-		return numero %3 = 0;
-	}
+            System.out.println("Selecciona una opción:");
+            System.out.println("1. ¿Es divisible entre 3?");
+            System.out.println("2. ¿Es divisible entre 5?");
+            System.out.println("3. ¿Es un número par?");
+            System.out.println("4. Salir");
+
+            int opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("¿Es divisible entre 3?: " + dividir3(numero));
+                    break;
+                case 2:
+                    System.out.println("¿Es divisible entre 5?: " + esDivisiblePorCinco(numero));
+                    break;
+                case 3:
+                    System.out.println("¿Es par?: " + esPar(numero));
+                    break;
+                case 4:
+                    System.out.println("Saliendo del programa...");
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("Opción no válida");
+                    break;
+            }
+        }
+
+        scanner.close();
+    }
+
+    public static boolean dividir3(int numero) {
+        return numero % 3 == 0;
+    }
 
     public static boolean esDivisiblePorCinco(int numero) {
         return numero % 5 == 0;
     }
-	public static boolean esPar (int numero){
-		return numero %2 == 0;
-	}
-}
 
+    public static boolean esPar(int numero) {
+        return numero % 2 == 0;
+    }
+}
--- src/Equipo05/Testdividir3.java
+++ src/Equipo05/Testdividir3.java
@@ -1,8 +1,11 @@
 package Equipo05;
 
-public class Main {
-	System.out.println("3 es divisible entre 3" + Main.dividir3(3));
-	System.out.println("3 es divisible entre 3" + Main.dividir3(0));
-	System.out.println("3 es divisible entre 3" + Main.dividir3(8));
-	System.out.println("3 es divisible entre 3" + Main.dividir3(9));
+public class Testdividir3 {
+	public static void main(String[] args) {
+		System.out.println("3 es divisible entre 3" + Main.dividir3(3));
+		System.out.println("3 es divisible entre 3" + Main.dividir3(0));
+		System.out.println("3 es divisible entre 3" + Main.dividir3(8));
+		System.out.println("3 es divisible entre 3" + Main.dividir3(9));
+	}
+
 }
